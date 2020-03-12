1dcef3d61d61781fe0b17c5cf20b6117b37f021f
Amr-Reda <amrreda547@gmail.com>
2020-03-12 18:58:06 +0200

    test

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ try {
     console.log('====================================');
     core.setOutput("NOT_USED_PACKAGES", "test");
     
-    const testFolder = __dirname + '/package.json';
+    const testFolder = './';
     // console.log('github.context.workflow ', github.context.);
     // console.log('github.context.repo ', github.context.repo.repo);
-    const content = fs.readFileSync(testFolder, 'utf8')
-    console.log('=ff===================================');
-    console.log(content);
-    console.log('====================================');
-    // fs.readdirSync(testFolder).forEach(file => {
-    //   console.log('file ',file);
-    // });
+    // const content = fs.readFileSync(testFolder, 'utf8')
+    // console.log('=ff===================================');
+    // console.log(content);
+    // console.log('====================================');
+    fs.readdirSync(testFolder).forEach(file => {
+      console.log('file ',file);
+    });
     // fs.readdirSync('/').forEach(file => {
     //   console.log(file);
     // });
